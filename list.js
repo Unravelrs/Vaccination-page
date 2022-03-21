5bb47475cce6b2722a88c375f3d67298c45f4906
Unravelrs <unravel.rs@gmail.com>
2022-03-20 19:13:04 -0600

    Agrego archivos

---
+++ list.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	var estado = false;
+
+	$('#btn-toggle.1').on('click',function(){
+		$('.avoid.1 p').slideToggle();
+        });
+
+        $('#btn-toggle.2').on('click',function(){
+            $('.avoid.2 p').slideToggle();
+            });
+
+         $('#btn-toggle.3').on('click',function(){
+                $('.avoid.3 p').slideToggle();
+                });
+
+                $('#btn-toggle.4').on('click',function(){
+                    $('.avoid.4 p').slideToggle();
+                    });
+
+                    $('#btn-toggle.5').on('click',function(){
+                        $('.avoid.5 p').slideToggle();
+                        });
+    });
